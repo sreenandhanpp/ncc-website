b9ae37d14d5d4e4e35af826e8642718c3d20c919
sreenandhanpp <sreedeviv27801@gamil.com>
2022-12-27 12:52:22 +0530

    navbar-changed

---
+++ src/components/FlagSection.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function FlagSection() {
+  return (
+    <div>
+        <div className='row'>
+            <div className='col-md-4'>
+                <img src='/public/images/flag.png' alt='NCC FLAG'></img>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default FlagSection
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 function Header() {
   return (
-    <div>
-         <nav class="navbar fixed-top navbar-expand-lg navbar-white p-md-3">
+    <div >
+         <nav class="navbar fixed-top navbar-expand-lg navbar-custom  ">
       <div class="container">
 
         <button
-          class="navbar-toggler"
+          class="navbar-toggler custom-toggler bg-white"
           type="button"
           data-bs-toggle="collapse"
           data-bs-target="#navbarNav"
@@ -19,24 +19,24 @@ function Header() {
         </button>
 
         <div class="collapse navbar-collapse" id="navbarNav">
-          <div class="mx-auto "></div>
-          <ul class="navbar-nav">
+          <div class=""></div>
+          <ul class="navbar-nav mx-auto">
             <li class="nav-item">
-              <a class="nav-link text-dark" href="/home">Home</a>
+              <a class="nav-link text-white" href="/home">Home</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link text-dark" href="/about">About</a>
+              <a class="nav-link text-white" href="/about">About</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link text-dark" href="/blog">Achievements</a>
+              <a class="nav-link text-white" href="/blog">Achievements</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link text-dark" href="/blog">Activity</a>
+              <a class="nav-link text-white" href="/blog">Activity</a>
             </li><li class="nav-item">
-              <a class="nav-link text-dark" href="/blog">Achievements</a>
+              <a class="nav-link text-white" href="/blog">Achievements</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link text-dark" href="/blog">Gallery</a>
+              <a class="nav-link text-white" href="/blog">Gallery</a>
             </li>
           </ul>
         </div>
