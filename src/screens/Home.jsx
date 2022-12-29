3b285191487de4768e92fad8c4b48d24965833b1
sreenandhanpp <sreedeviv27801@gamil.com>
2022-12-29 10:37:00 +0530

    added-about-page

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import About from '../components/About'
 import Header from '../components/Header'
 import Slide from '../components/Slide'
 
@@ -7,6 +8,7 @@ function Home() {
     <div>
         <Header />
         <Slide />
+        <About />
     </div>
   )
 }
